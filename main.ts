46fb27cc9c81495c45f6202c14e9b35a89c5510f
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2016-12-10 18:41:11 -0500

    Processing Mouse Events

--- main.ts
+++ main.ts
@@ -1,73 +1,23 @@
-/* should do something similar to what's below in prod do reduce app size instead of general import */
-// import {Observable} from 'rxjs/Observable';
-// import {Observer} from 'rxjs/Observer';
+import {Observable} from 'rxjs';
 
-// import 'rxjs/add/observable/from';
-// import 'rxjs/add/operator/filter';
-// import 'rxjs/add/operator/map';
-import {Observable, Observer} from 'rxjs';
-
-const numbers = [1, 5, 10];
-let source: Observable<any>;
-
-function createObservableFrom(): void {
-  class MyObserver implements Observer<number> {
-    next(value: any): void {
-      console.log(`value: ${value}`);
-    }
-
-    error(e: any): void {
-      console.log(`error: ${e}`);
-    }
-
-    complete(): void {
-      console.log('complete');
-    }
-  }
-
-  source = Observable.from(numbers);
-  // source.subscribe(new MyObserver());
-}
-
-function createObservableCreate(): void {
-  source = Observable.create(observer => {
-    for (let n of numbers) {
-      if (n === 5) {
-        observer.error('Something went wrong!');
-      }
-
-      observer.next(n);
-    }
-
-    observer.complete();
-  });
-}
-
-function observableOverTime(): void {
-  source = Observable.create(observer => {
-    let index = 0;
-    let produceValue = () => {
-      observer.next(numbers[index++]);
-
-      if (index < numbers.length) {
-        setTimeout(produceValue, 250);
-      } else {
-        observer.complete();
-      }
-    };
-
-    produceValue();
+let circle = document.getElementById('circle');
+let source = Observable.fromEvent(document, 'mousemove')
+  .map((e: MouseEvent) => {
+    return ({
+      x: e.clientX,
+      y: e.clientY,
+    });
   })
-  .map(n => n * 2)
-  .filter(n => n > 4);
-}
+  .filter(value => value.x < 500)
+  .delay(300);
 
-// createObservableFrom();
-// createObservableCreate();
-observableOverTime();
+function onNext(value: any): void {
+  circle.style.left = `${value.x}px`;
+  circle.style.top = `${value.y}px`;
+}
 
 source.subscribe(
-  value => console.log(`value: ${value}`),
+  onNext,
   e => console.log(`error: ${e}`),
   () => console.log('complete'),
 );
