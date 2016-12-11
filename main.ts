cdfffecb52096cc2b44cf2c04c7fed03c147e04e
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2016-12-10 19:34:23 -0500

    Implementing Retry Logic

--- main.ts
+++ main.ts
@@ -9,14 +9,30 @@ function load(url: string): Observable<any> {
     const xhr = new XMLHttpRequest();
 
     xhr.addEventListener('load', () => {
-      const data: any[] = JSON.parse(xhr.responseText);
-      observer.next(data);
-      observer.complete();
+      if (xhr.status === 200) {
+        const data: any[] = JSON.parse(xhr.responseText);
+        observer.next(data);
+        observer.complete();
+      } else {
+        observer.error(xhr.statusText);
+      }
     });
 
     xhr.open('GET', url);
     xhr.send();
-  });
+  }).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
+}
+
+function retryStrategy({attempts = 4, delay = 1000}: any): Function {
+  return (errors: Observable<string>): Observable<any> => {
+    return errors
+      .scan((acc, value) => {
+        console.log(acc, value);
+        return ++acc;
+      }, 0)
+      .takeWhile(acc => acc < attempts)
+      .delay(delay);
+  };
 }
 
 function renderMovies(movies: any[]): void {
